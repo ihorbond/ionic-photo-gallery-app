9f7fcace84a5b77eb144f21ea4bed05555c302e5
Ihor Bodnarchuk <Ihor.Bodnarchuk@fsresidential.com>
2020-04-23 15:06:43 -0400

    add ios and android

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Plugins, CameraResultType, Capacitor, FilesystemDirectory, 
-  CameraPhoto, CameraSource } from '@capacitor/core';
+import {
+  Plugins, CameraResultType, Capacitor, FilesystemDirectory,
+  CameraPhoto, CameraSource, FileReadResult
+} from '@capacitor/core';
+import { Platform } from '@ionic/angular';
 
 const { Camera, Filesystem, Storage } = Plugins;
 const PHOTO_STORAGE: string = "photos";
@@ -10,22 +13,36 @@ const PHOTO_STORAGE: string = "photos";
 })
 export class PhotoService {
   public photos: Photo[] = [];
+  private platform: Platform;
 
-  constructor() { }
-
-  private async readAsBase64(cameraPhoto: CameraPhoto) {
-    // Fetch the photo, read as a blob, then convert to base64 format
-    const response = await fetch(cameraPhoto.webPath!);
-    const blob = await response.blob();
-  
-    return await this.convertBlobToBase64(blob) as string;  
+  constructor(platform: Platform) {
+    this.platform = platform;
   }
-  
+
+  private async readAsBase64(cameraPhoto: CameraPhoto): Promise<string> {
+    // "hybrid" will detect Cordova or Capacitor
+    if (this.platform.is('hybrid')) {
+      // Read the file into base64 format
+      const file: FileReadResult = await Filesystem.readFile({
+        path: cameraPhoto.path
+      });
+
+      return file.data;
+    }
+    else {
+      const response = await fetch(cameraPhoto.webPath);
+      const blob = await response.blob();
+
+      return await this.convertBlobToBase64(blob) as string;
+    }
+
+  }
+
   convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
     const reader = new FileReader;
     reader.onerror = reject;
     reader.onload = () => {
-        resolve(reader.result);
+      resolve(reader.result);
     };
     reader.readAsDataURL(blob);
   });
@@ -40,9 +57,21 @@ export class PhotoService {
       directory: FilesystemDirectory.Data
     });
 
-    return {
-      filePath: fileName,
-      webViewPath: cameraPhoto.webPath
+    if (this.platform.is('hybrid')) {
+      // Display the new image by rewriting the 'file://' path to HTTP
+      // Details: https://ionicframework.com/docs/building/webview#file-protocol
+      return {
+        filePath: savedFile.uri,
+        webViewPath: Capacitor.convertFileSrc(savedFile.uri)
+      };
+    }
+    else {
+      // Use webPath to display the new image instead of base64 since it's
+      // already loaded into memory
+      return {
+        filePath: fileName,
+        webViewPath: cameraPhoto.webPath
+      };
     }
   }
 
@@ -58,30 +87,55 @@ export class PhotoService {
 
     Storage.set({
       key: PHOTO_STORAGE,
-      value: JSON.stringify(this.photos.map(p => {
-        const photoCopy = { ...p };
-        delete photoCopy.base64;
+      value: this.platform.is('hybrid')
+        ? JSON.stringify(this.photos)
+        : JSON.stringify(this.photos.map(p => {
+          const photoCopy = { ...p };
+          delete photoCopy.base64;
 
-        return photoCopy;
-      }))
+          return photoCopy;
+        }))
     })
   }
 
   public async loadSaved(): Promise<void> {
     const photos = await Storage.get({ key: PHOTO_STORAGE });
     this.photos = JSON.parse(photos.value) || [];
-  
-    // Display the photo by reading into base64 format
-    this.photos.forEach(async p => {
-      const readFile = await Filesystem.readFile({
-        path: p.filePath,
-        directory: FilesystemDirectory.Data
+
+    if (!this.platform.is('hybrid')) {
+      // Display the photo by reading into base64 format
+      this.photos.forEach(async p => {
+        const readFile = await Filesystem.readFile({
+          path: p.filePath,
+          directory: FilesystemDirectory.Data
+        });
+
+        // Web platform only: Save the photo into the base64 field
+        p.base64 = `data:image/jpeg;base64,${readFile.data}`;
       });
+    }
+  }
+
+  public async deletePicture(photo: Photo, position: number): Promise<void> {
+    // Remove this photo from the Photos reference data array
+    this.photos.splice(position, 1);
   
-      // Web platform only: Save the photo into the base64 field
-      p.base64 = `data:image/jpeg;base64,${readFile.data}`;
+    // Update photos array cache by overwriting the existing photo array
+    Storage.set({
+      key: PHOTO_STORAGE,
+      value: JSON.stringify(this.photos)
+    });
+  
+    // delete photo file from filesystem
+    const filename = photo.filePath
+                        .substr(photo.filePath.lastIndexOf('/') + 1);
+  
+    await Filesystem.deleteFile({
+      path: filename,
+      directory: FilesystemDirectory.Data
     });
   }
+
 }
 
 interface Photo {
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {PhotoService} from '../services/photo.service';
+import { PhotoService } from '../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -8,10 +9,35 @@ import {PhotoService} from '../services/photo.service';
 })
 export class Tab2Page implements OnInit {
 
-  constructor(public photoService: PhotoService) {}
+  constructor(
+    public photoService: PhotoService,
+    public actionSheetController: ActionSheetController
+  ) { }
 
   ngOnInit(): void {
     this.photoService.loadSaved();
   }
-  
+
+  public async showActionSheet(photo, position: number): Promise<void> {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
+  }
+
 }
